04590ea068e6d6b9b3cbbb510c34cc3088bb604f
HermanL02 <Hermanyiqunliang@gmail.com>
2025-06-26 18:03:11 -0300

    fix: cors

--- src/web.ts
+++ src/web.ts
@@ -8,6 +8,19 @@ const PORT = 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Disable CORS - allow all origins
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // Simple HTML page
 const HTML_PAGE = `
 <!DOCTYPE html>
